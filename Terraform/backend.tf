2683ae20072863c959fd890a5baf5c8cf92ae3df
Connor <cgedi24@outlook.com>
2026-04-29 09:18:58 -0500

    added backend

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,10 +1,8 @@
-# backend.tf
 # Configure Terraform to use Azure Storage for state file
-
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-hubspoke-lab"
-    storage_account_name = "tfstate3847"
+    resource_group_name  = "rghubspoke-tfstate"
+    storage_account_name = "tfstateconnor12345"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
